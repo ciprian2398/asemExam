7a1a244c282b6e60b70a4ad0164cc1a84ab4f4c5
ciprian <ciprian2398@gmial.com>
2019-01-30 22:52:46 +0200

    final commit

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Input sentence to find \"info\"");
-        System.out.println(new Scanner(System.in).nextLine().contains("info") ? "Sentence contains \"info\"" : "Sentence don\'t contains \"info\"");
+        System.out.println(new Scanner(System.in).nextLine().contains("info")
+                ? "Sentence contains \"info\""
+                : "Sentence don\'t contains \"info\"");
     }
 }
